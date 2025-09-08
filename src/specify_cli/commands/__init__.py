1b95b15222e13bad88d95f3088b2376998331364
barisgit <baristovnik@gmail.com>
2025-09-08 08:48:25 +0200

    Add update and version check functionality

--- src/specify_cli/commands/__init__.py
+++ src/specify_cli/commands/__init__.py
@@ -2,5 +2,6 @@
 
 from .check import check_command
 from .init import init_command
+from .update import update_app
 
-__all__ = ["check_command", "init_command"]
+__all__ = ["check_command", "init_command", "update_app"]
